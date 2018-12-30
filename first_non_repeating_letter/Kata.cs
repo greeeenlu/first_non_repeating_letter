d86acf7d16607b3fb805e6e213705ba2ee1b223a
green.lu <green.lu@titansoft.com.sg>
2018-12-30 23:05:42 +0800

    1. inline variable 2. move class into new file

---
+++ first_non_repeating_letter/Kata.cs
@@ -0,0 +1,10 @@
+﻿namespace first_non_repeating_letter
+{
+    public class Kata
+    {
+        public static string FirstNonRepeatingLetter(string word)
+        {
+            return word;
+        }
+    }
+}
--- first_non_repeating_letter/KataTest.cs
+++ first_non_repeating_letter/KataTest.cs
@@ -9,16 +9,7 @@ namespace first_non_repeating_letter
         [TestMethod]
         public void only_one_letter_return_this_one_letter()
         {
-            var actual = Kata.FirstNonRepeatingLetter("a");
-            Assert.AreEqual("a",actual);
-        }
-    }
-
-    public class Kata
-    {
-        public static string FirstNonRepeatingLetter(string word)
-        {
-            return word;
+            Assert.AreEqual("a",Kata.FirstNonRepeatingLetter("a"));
         }
     }
 }
