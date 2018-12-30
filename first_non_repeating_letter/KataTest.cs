dd761dca6f2030e291333e42568c88b4bb1832a2
green.lu <green.lu@titansoft.com.sg>
2018-12-30 23:02:29 +0800

    case : only one letter pass

---
+++ first_non_repeating_letter/KataTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace first_non_repeating_letter
+{
+    [TestClass]
+    public class KataTest
+    {
+        [TestMethod]
+        public void only_one_letter_return_this_one_letter()
+        {
+            var actual = Kata.FirstNonRepeatingLetter("a");
+            Assert.AreEqual("a",actual);
+        }
+    }
+
+    public class Kata
+    {
+        public static string FirstNonRepeatingLetter(string word)
+        {
+            return word;
+        }
+    }
+}
